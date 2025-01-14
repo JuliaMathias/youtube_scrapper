cd989979b8fc98acaf769054544ea477335fba4e
JuliaMathias <juliamss@gmail.com>
2025-01-14 19:58:38 -0300

    Add Video schema and migration for videos table

---
+++ lib/you_tube_scrapper/playlists/video.ex
@@ -0,0 +1,23 @@
+defmodule YouTubeScrapper.Playlists.Video do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "videos" do
+    field :description, :string
+    field :title, :string
+    field :duration, :string
+    field :scraped_on, :date
+    field :playlist_id, :binary_id
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(video, attrs) do
+    video
+    |> cast(attrs, [:title, :duration, :description, :scraped_on])
+    |> validate_required([:title, :duration, :description, :scraped_on])
+  end
+end
