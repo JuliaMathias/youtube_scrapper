f4125a33273c281faa8f240a1b68bf76a923eb26
JuliaMathias <juliamss@gmail.com>
2025-01-14 16:31:34 -0300

    update elixir version to 1.18 and adjust heroicons git depth

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule YouTubeScrapper.MixProject do
     [
       app: :you_tube_scrapper,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
